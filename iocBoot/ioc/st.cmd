7c4c91a301e38b39b59405fe84cd7e3a028f5a6f
Bryan Moffit <moffit@jlab.org>
2022-12-04 22:09:59 -0500

    Add example iocBoot

---
+++ iocBoot/ioc/st.cmd
@@ -0,0 +1,22 @@
+#!../../bin/linux-x86_64/VLD
+
+
+< envPaths
+
+epicsEnvSet("PORT", "daqlab")
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/VLD.dbd"
+VLD_registerRecordDeviceDriver pdbbase
+
+VLDConfig("$(PORT)",0x380000,0,1,0);
+
+## Load record instances
+dbLoadTemplate($(TOP)/db/"VLD-$(PORT).substitutions","P=$(PORT),PORT=$(PORT)")
+
+asynSetTraceMask("$(PORT)", 7, ASYN_TRACE_FLOW|ASYN_TRACE_DRIVER)
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
